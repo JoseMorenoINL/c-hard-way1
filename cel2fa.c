99a82038740a0b649ec53fe123a153acc156bfa0
Jose Moreno <josemoreno.inl@gmail.com>
2021-11-16 18:24:44 -0400

    Cel2Fah

---
+++ cel2fa.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (int argc, char *argv[])
+{
+    float fahr, celsius;
+    int lower, upper, step;
+    lower = 0;
+    upper =300;
+    step =20;
+    fahr = lower;
+    printf("Fahrenheit \t Celsius\n");
+    while (fahr <= upper){
+        celsius = 5.0/9.0 * (fahr-32.0);
+        
+        printf("%3.0f\t %6.2f\n", fahr,celsius);
+        fahr = fahr + step;
+    }
+    return 0;
+}
+
